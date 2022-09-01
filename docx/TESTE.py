cc38ce100b512b0c8679bfcc4e753180e0361df4
Vitor Silva <silvadavitor999@gmail.com>
2022-09-01 15:26:09 -0300

    Atualização 2.0

--- docx/TESTE.py
+++ docx/TESTE.py
@@ -5,12 +5,12 @@ dirPath = r"C:\Users\entra21\Desktop\testes"
 result = next(os.walk(dirPath))[2]
 for j in result:
     texto = docx2txt.process(fr"C:\Users\entra21\Desktop\testes\{j}").lower()
-    palavras = ['vitor da silva', 'ave', 'maria', 'blumenau', 'beautiful']
+    palavras = ['python', 'inglês']
 
     for i in palavras:
         if i in texto:
             print(f'DOCX({j}) - Palavra "{i}": ✔')
-            print(fr"C:\Users\entra21\Desktop\testes\{j}")
+            # print(fr"C:\Users\entra21\Desktop\testes\{j}")
         else:
             print(f'DOCX({j}) - Palavra "{i}": ❌')
     print()
--- teste2.py
+++ teste2.py
@@ -1,24 +1,21 @@
 import os
-
 import fitz
 
-
-dirPath = r"C:\Users\entra21\Desktop\testes"
+dirPath = r"C:\Users\entra21\Desktop\teste pdf"
 result = next(os.walk(dirPath))[2]
 
 for j in result:
-    palavras = ['vitor', 'silva', 'maria', 'python', 'beautiful']
+    palavras = ['Python', 'Inglês']
 
-    DIGITIZED_FILE = fr"C:\Users\entra21\Desktop\testes\{j}".lower()
-    SCANNED_FILE = fr"C:\Users\entra21\Desktop\testes\{j}".lower()
+    DIGITIZED_FILE = fr"C:\Users\entra21\Desktop\teste pdf\{j}".lower()
     try:
-        with fitz.open(DIGITIZED_FILE) as doc:
-           for page in doc:
-               text = page.get_text()
-               for palavra in palavras:
-                   if palavra in text:
-                       print(f'PDF - Palavra "{palavra}": ✔')
-                   else:
-                       print(f'PDF - Palavra "{palavra}": ❌')
+        with fitz.open(DIGITIZED_FILE) as pdf:
+            for page in pdf:
+                text = page.get_text()
+                for palavra in palavras:
+                    if palavra in text:
+                        print(f'PDF({j}) - Palavra "{palavra}": ✔')
+                    else:
+                        print(f'PDF({j}) - Palavra "{palavra}": ❌')
     except:
         pass
