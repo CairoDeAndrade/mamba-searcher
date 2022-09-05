d8e823ddd9e1368c9e94b8c9f43b004475a54526
CairoDeAndrade <cairo.andrade777@gmail.com>
2022-09-05 17:27:56 -0300

    Start

---
+++ tcc_test/_test/vcode_test/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+
+def index(request):
+    import docx2txt
+    import os
+
+    dirPath = r"C:\Users\entra21\PycharmProjects\TCC\tcc-entra21\docx\testes"
+
+    result = next(os.walk(dirPath))[2]
+
+    for j in result:
+        texto = docx2txt.process(fr"C:\Users\entra21\PycharmProjects\TCC\tcc-entra21\docx\testes\{j}").lower()
+        palavras = ['vitor da silva', 'ave', 'maria', 'blumenau', 'beautiful']
+
+        for i in palavras:
+            if i in texto:
+                print(f'DOCX({j}) - Palavra "{i}": ✔')
+                print(fr"C:\Users\entra21\Desktop\testes\{j}")
+            else:
+                print(f'DOCX({j}) - Palavra "{i}": ❌')
+    return render(request, 'vcode_test/index.html')
