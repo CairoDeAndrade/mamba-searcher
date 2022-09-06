0498caddc1539274de329366af60677aeeb64b14
CairoDeAndrade <cairo.andrade777@gmail.com>
2022-09-06 17:26:41 -0300

    django test
    vitor code

--- tcc_test/_test/vcode_test/views.py
+++ tcc_test/_test/vcode_test/views.py
@@ -1,22 +1,16 @@
 from django.shortcuts import render
+import docx2txt
+import os
 
 
 def index(request):
-    import docx2txt
-    import os
-
-    dirPath = r"C:\Users\entra21\PycharmProjects\TCC\tcc-entra21\docx\testes"
-
+    dirPath = r"C:\Users\entra21\Desktop\tcc_test"
     result = next(os.walk(dirPath))[2]
 
-    for j in result:
-        texto = docx2txt.process(fr"C:\Users\entra21\PycharmProjects\TCC\tcc-entra21\docx\testes\{j}").lower()
-        palavras = ['vitor da silva', 'ave', 'maria', 'blumenau', 'beautiful']
+    for i in result:
+        texto = docx2txt.process(fr"C:\Users\entra21\Desktop\tcc_test\{i}").lower()
+        palavras = ['python', 'inglês']
 
-        for i in palavras:
-            if i in texto:
-                print(f'DOCX({j}) - Palavra "{i}": ✔')
-                print(fr"C:\Users\entra21\Desktop\testes\{j}")
-            else:
-                print(f'DOCX({j}) - Palavra "{i}": ❌')
-    return render(request, 'vcode_test/index.html')
+        return render(request, 'vcode_test/index.html', {'texto': texto,
+                                                         'palavras': palavras,
+                                                         'result': result})
