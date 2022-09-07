1b21ce6a63bf4f8a81c6a5b74f000ffdfd587e8b
Cairo Augusto de Andrade <cairo.andrade777@gmail.com>
2022-09-06 22:44:39 -0300

    atualização de casa

---
+++ tcc_test/_test/vcode_test/templatetags/vitor_code.py
@@ -0,0 +1,10 @@
+# import docx2txt
+# import os
+#
+# dirPath = r"C:\Users\entra21\Desktop\testes"
+# result = next(os.walk(dirPath))[2]
+# for j in result:
+#     texto = docx2txt.process(fr"C:\Users\entra21\Desktop\testes\{j}").lower()
+#     palavras = ['python', 'inglês']
+
+
--- tcc_test/_test/vcode_test/views.py
+++ tcc_test/_test/vcode_test/views.py
@@ -4,13 +4,15 @@ import os
 
 
 def index(request):
-    dirPath = r"C:\Users\entra21\Desktop\tcc_test"
+    dirPath = r"C:\Users\cairo\OneDrive\Área de Trabalho\tcc_test"
     result = next(os.walk(dirPath))[2]
 
+    texto = []
+    palavras = []
     for i in result:
-        texto = docx2txt.process(fr"C:\Users\entra21\Desktop\tcc_test\{i}").lower()
+        texto = docx2txt.process(fr"C:\Users\cairo\OneDrive\Área de Trabalho\tcc_test\{i}").lower()
         palavras = ['python', 'inglês']
 
-        return render(request, 'vcode_test/index.html', {'texto': texto,
-                                                         'palavras': palavras,
-                                                         'result': result})
+    return render(request, 'vcode_test/index.html', {'texto': texto,
+                                                     'palavras': palavras,
+                                                     'result': result})
