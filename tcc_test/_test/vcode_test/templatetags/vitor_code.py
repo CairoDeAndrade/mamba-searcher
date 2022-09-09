bf3412fe7ddaa81fa7d229fd855bc52c962204ae
CairoDeAndrade <cairo.andrade777@gmail.com>
2022-09-09 13:29:23 -0300

    upload

--- tcc_test/_test/vcode_test/templatetags/vitor_code.py
+++ tcc_test/_test/vcode_test/templatetags/vitor_code.py
@@ -1,10 +1,15 @@
-# import docx2txt
-# import os
-#
-# dirPath = r"C:\Users\entra21\Desktop\testes"
-# result = next(os.walk(dirPath))[2]
-# for j in result:
-#     texto = docx2txt.process(fr"C:\Users\entra21\Desktop\testes\{j}").lower()
-#     palavras = ['python', 'inglês']
-#
-#
+from django.shortcuts import render
+import os
+
+
+def index(request):
+    global i
+    dirPath = r"C:\Users\entra21\Desktop\tcc_test"
+    result = next(os.walk(dirPath))[2]
+
+    num = 0
+    for j in range(len(result)):
+        num += 1
+
+        return render(request, 'vcode_test/index.html', {'num': num,
+                                                             })
--- tcc_test/_test/vcode_test/views.py
+++ tcc_test/_test/vcode_test/views.py
@@ -5,17 +5,18 @@ import os
 
 def index(request):
     global i
-    dirPath = r"C:\Users\cairo\OneDrive\Área de Trabalho\tcc_test"
+    # dirPath = r"C:\Users\cairo\OneDrive\Área de Trabalho\tcc_test" home
+    dirPath = r"C:\Users\entra21\Desktop\tcc_test"
     result = next(os.walk(dirPath))[2]
 
     doc_texto, texto, palavras_chave,  = [], [], []
-    position, num = 0, 1
     for i in result:
-        texto = docx2txt.process(fr"C:\Users\cairo\OneDrive\Área de Trabalho\tcc_test\{i}").lower()
+        # texto = docx2txt.process(fr"C:\Users\cairo\OneDrive\Área de Trabalho\tcc_test\{i}").lower() home
+        texto = docx2txt.process(fr"C:\Users\entra21\Desktop\tcc_test\{i}").lower()
         doc_texto.append(texto)
         palavras_chave = ['python', 'inglês']
 
     return render(request, 'vcode_test/index.html', {'texto': texto,
                                                      'palavras_chave': palavras_chave,
                                                      'result': result, 'doc_texto': doc_texto,
-                                                     'position': position, 'num': num})
+                                                     })
