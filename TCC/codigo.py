dfb21437053ee67d62c5a42d1a74e4e7b9c624e7
cstrey <cauanstrey@gmail.com>
2022-09-23 17:04:18 -0300

    home

--- TCC/codigo.py
+++
@@ -1,8 +0,0 @@
-import fitz
-
-DIGITIZED_FILE = "pdffoda.pdf.pdf"
-SCANNED_FILE = "pdffoda.pdf.pdf"
-with fitz.open(DIGITIZED_FILE) as doc:
-   for page in doc:
-       text = page.get_text()
-       print(text)
--- TCC/pdf.py
+++
@@ -1,8 +0,0 @@
-from PyPDF2 import PdfReader
-
-reader = PdfReader("2021-03-19_exemplo-pdf.pdf")
-number_of_pages = len(reader.pages)
-page = reader.pages[0]
-text = page.extract_text()
-print(text)
-print("Ao clica com o botão" in text)
