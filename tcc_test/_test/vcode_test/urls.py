719c3038aede939f520239a1f090200780605da1
CairoDeAndrade <cairo.andrade777@gmail.com>
2022-09-23 14:59:16 -0300

    atualização 2.4

--- tcc_test/_test/vcode_test/urls.py
+++ tcc_test/_test/vcode_test/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     # path('', views.index),
-    path('mamba/', views.mamba),
+    path('mamba/', views.mamba, name='mamba'),
+    # path('search/', views.search, name='search'),
     ]
--- tcc_test/_test/vcode_test/views.py
+++ tcc_test/_test/vcode_test/views.py
@@ -7,14 +7,14 @@ from operator import itemgetter
 
 
 def mamba(request, ):
-    # dirPath = r"C:\Users\entra21\Desktop\testes"
-    dirPath = r"C:\Users\cairo\OneDrive\Área de Trabalho\testes"  # Home
+    dirPath = r"C:\Users\entra21\Desktop\testes"
+    # dirPath = r"C:\Users\cairo\OneDrive\Área de Trabalho\testes"  # Home
     lista_arquivos = next(os.walk(dirPath))[2]
 
     lista_quantidade_palavras, texto, lista_final, list_files_name, list_word_qtd, real_final = [], [], [], [], [], []
     total = 0
-    caminho = ''
-    result, contained_words = [], []
+    caminho, contained_words = '', ''
+    result, list_contained_words, final_contained_words = [], [], []
     final_dict = {}
     palavras_chave = 'python,ingles'
     novas_palavras = ''.join(ch for ch in unicodedata.normalize('NFKD', palavras_chave).lower()
@@ -23,8 +23,8 @@ def mamba(request, ):
 
     for i in lista_arquivos:
         try:
-            # caminho = fr"C:\Users\entra21\Desktop\testes\{i}"
-            caminho = fr"C:\Users\cairo\OneDrive\Área de Trabalho\testes\{i}"  # home
+            caminho = fr"C:\Users\entra21\Desktop\testes\{i}"
+            # caminho = fr"C:\Users\cairo\OneDrive\Área de Trabalho\testes\{i}"  # home
             sum = 0
             texto = docx2txt.process(caminho)
             novo_texto = ''.join(ch for ch in unicodedata.normalize('NFKD', texto).lower()
@@ -35,10 +35,14 @@ def mamba(request, ):
             for palavra in novas_palavras:
                 if palavra in novo_texto:
                     sum += 1
-                    contained_words.append(palavra)
+                    contained_words += f'{palavra}, '
                 else:
                     continue
+
+            list_contained_words.append(contained_words)
+            contained_words = ''
             lista_quantidade_palavras.append(sum)
+
         except:
             # quantidade = []
             text = ''
@@ -52,13 +56,17 @@ def mamba(request, ):
             for j in novas_palavras:
                 if j in new_text:
                     sum += 1
-                    contained_words.append(j)
+                    contained_words += f'{j}, '
                 else:
                     continue
+
+            list_contained_words.append(contained_words)
+            contained_words = ''
             lista_quantidade_palavras.append(sum)
 
-    for arquivo, quantidade_palavras in zip(lista_arquivos, lista_quantidade_palavras):
-        final_dict = {'arquivo': arquivo, 'quantidade_palavras': quantidade_palavras}
+    for arquivo, quantidade_palavras, palavras_contidas in zip(lista_arquivos, lista_quantidade_palavras, list_contained_words):
+        final_dict = {'arquivo': arquivo, 'quantidade_palavras': quantidade_palavras,
+                      'palavras_contidas': palavras_contidas}
 
         # quantidade = [arquivo, quantidade_palavras, total]
         lista_final.append(final_dict)
@@ -67,15 +75,16 @@ def mamba(request, ):
     for i in result:
         list_files_name.append(i['arquivo'])
         list_word_qtd.append(i['quantidade_palavras'])
+        final_contained_words.append(i['palavras_contidas'])
 
-    for files_name, word_qtd, word in zip(list_files_name, list_word_qtd, contained_words):
-        complete_list = [files_name, word_qtd, word]
+    for files_name, word_qtd, words in zip(list_files_name, list_word_qtd, final_contained_words):
+        complete_list = [files_name, word_qtd, words]
         real_final.append(complete_list)
 
-    return render(request, 'vcode_test/mamba.html', {'novas_palavras': novas_palavras,
-                                                     'real_final': real_final, 'total': total,
-                                                     'contained_words': contained_words,
+    return render(request, 'vcode_test/mamba.html', {'real_final': real_final, 'total': total,
+                                                     # 'list_contained_words': list_contained_words,
                                                      })
+
 
 #
 # def index(request):
