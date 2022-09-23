ddcf8008b8edb27f52fa513fcd80d4e415cf4108
Cairo Augusto de Andrade <cairo.andrade777@gmail.com>
2022-09-23 09:48:45 -0300

    começando

--- tcc_test/_test/vcode_test/views.py
+++ tcc_test/_test/vcode_test/views.py
@@ -14,8 +14,8 @@ def mamba(request, ):
     lista_quantidade_palavras, texto, lista_final, list_files_name, list_word_qtd, real_final = [], [], [], [], [], []
     total = 0
     caminho = ''
-    result, final_dict = [], {}
-
+    result, contained_words = [], []
+    final_dict = {}
     palavras_chave = 'python,ingles'
     novas_palavras = ''.join(ch for ch in unicodedata.normalize('NFKD', palavras_chave).lower()
                              if not unicodedata.combining(ch))
@@ -24,7 +24,7 @@ def mamba(request, ):
     for i in lista_arquivos:
         try:
             # caminho = fr"C:\Users\entra21\Desktop\testes\{i}"
-            caminho = fr"C:\Users\cairo\OneDrive\Área de Trabalho\testes\{i}" # home
+            caminho = fr"C:\Users\cairo\OneDrive\Área de Trabalho\testes\{i}"  # home
             sum = 0
             texto = docx2txt.process(caminho)
             novo_texto = ''.join(ch for ch in unicodedata.normalize('NFKD', texto).lower()
@@ -35,11 +35,12 @@ def mamba(request, ):
             for palavra in novas_palavras:
                 if palavra in novo_texto:
                     sum += 1
+                    contained_words.append(palavra)
                 else:
                     continue
             lista_quantidade_palavras.append(sum)
         except:
-            quantidade = []
+            # quantidade = []
             text = ''
             sum = 0
             with fitz.open(caminho) as doc:
@@ -51,14 +52,15 @@ def mamba(request, ):
             for j in novas_palavras:
                 if j in new_text:
                     sum += 1
+                    contained_words.append(j)
                 else:
                     continue
             lista_quantidade_palavras.append(sum)
 
     for arquivo, quantidade_palavras in zip(lista_arquivos, lista_quantidade_palavras):
-        final_dict = {'arquivo': arquivo, 'quantidade_palavras': quantidade_palavras, 'total': total}
+        final_dict = {'arquivo': arquivo, 'quantidade_palavras': quantidade_palavras}
 
-        quantidade = [arquivo, quantidade_palavras, total]
+        # quantidade = [arquivo, quantidade_palavras, total]
         lista_final.append(final_dict)
         result = sorted(lista_final, key=itemgetter('quantidade_palavras'), reverse=True)
 
@@ -66,13 +68,13 @@ def mamba(request, ):
         list_files_name.append(i['arquivo'])
         list_word_qtd.append(i['quantidade_palavras'])
 
-    for files_name, word_qtd in zip(list_files_name, list_word_qtd):
-        complete_list = [files_name, word_qtd]
+    for files_name, word_qtd, word in zip(list_files_name, list_word_qtd, contained_words):
+        complete_list = [files_name, word_qtd, word]
         real_final.append(complete_list)
 
-    return render(request, 'vcode_test/mamba.html', {'lista_final': lista_final,
-                                                     'novas_palavras': novas_palavras, 'result': result,
+    return render(request, 'vcode_test/mamba.html', {'novas_palavras': novas_palavras,
                                                      'real_final': real_final, 'total': total,
+                                                     'contained_words': contained_words,
                                                      })
 
 #
