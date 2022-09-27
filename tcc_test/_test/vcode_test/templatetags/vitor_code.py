310d987964276d54bfa47fe64d146051bccbdc55
Cairo Augusto de Andrade <cairo.andrade777@gmail.com>
2022-09-27 12:45:53 -0300

    css+ht

--- tcc_test/_test/vcode_test/templatetags/vitor_code.py
+++ tcc_test/_test/vcode_test/templatetags/vitor_code.py
@@ -1,15 +1,4 @@
-from django.shortcuts import render
-import os
+# from django.shortcuts import render
 
 
-def index(request):
-    global i
-    dirPath = r"C:\Users\entra21\Desktop\tcc_test"
-    result = next(os.walk(dirPath))[2]
 
-    num = 0
-    for j in range(len(result)):
-        num += 1
-
-        return render(request, 'vcode_test/index.html', {'num': num,
-                                                             })
