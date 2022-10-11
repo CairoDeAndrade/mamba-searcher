2f02568b1a31c3bc8c7fbbc3bd95b8fcc5260e9b
Cairo Augusto de Andrade <cairo.andrade777@gmail.com>
2022-10-11 17:20:13 -0300

    defs and register page

--- tcc_principal/super_searcher/urls.py
+++ tcc_principal/super_searcher/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
         path('search/', views.search, name='search'),
         path('ranking/', views.ranking, name='ranking'),
         path('about/', views.about, name='about'),
+        path('synonyms/', views.synonyms, name='synonyms'),
+        path('ranking_synonyms/', views.ranking_synonyms, name='ranking_synonyms'),
+        path('email_input/', views.email_input, name='email_input'),
+        path('email_response/', views.email_response, name='email_response'),
 ]
--- tcc_principal/super_searcher/views.py
+++ tcc_principal/super_searcher/views.py
@@ -1,7 +1,7 @@
 # Django imports
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 # Models imports
 from .models import File, FilteredFiles
 # Super-searcher imports
@@ -22,11 +22,6 @@ import shutil
 
 
 def home(request):
-    # dirPath = r"media/filtered_files"
-    # list_arq = next(os.walk(dirPath))[2]
-    # for i in list_arq:
-    #     os.remove(f"media/filtered_files/{i}")
-
     return render(request, 'super_searcher/home.html')
 
 
@@ -311,7 +306,118 @@ def real_final(request):
 
 
 def search(request):
+    # Removing the filtered files to make a new search
+    dirPath = r"media/filtered_files"
+    list_arq = next(os.walk(dirPath))[2]
+    for i in list_arq:
+        os.remove(f"media/filtered_files/{i}")
+
     return render(request, 'super_searcher/search.html')
+
+
+def synonyms(request):
+    return render(request, 'super_searcher/synonyms.html')
+
+
+def ranking_synonyms(request):
+    # If the search input is empty
+    term = request.GET.get('term')
+    if not term:
+        return redirect('synonyms')
+
+    # functionalities
+    # dirPath = r"C:\Users\entra21\Desktop\testes"
+    dirPath = r"media\files"  # vitor
+    # dirPath = r"C:\Users\cairo\OneDrive\Área de Trabalho\testes"  # cairo
+    lista_arquivos = next(os.walk(dirPath))[2]
+
+    lista_quantidade_palavras, texto, lista_final, list_files_name, list_word_qtd, real_final = [], [], [], [], [], []
+    total = 0
+    caminho, contained_words, no_synonyms = '', '', ''
+    result, list_contained_words, final_contained_words = [], [], []
+    final_dict = {}
+
+    # Using the synonyms to search
+    palavras_chave = str(request.GET.get('term')).lower()
+    search_word = ''.join(ch for ch in unicodedata.normalize('NFKD', palavras_chave).lower()
+                          if not unicodedata.combining(ch))
+    synonym_word = Search(f'{search_word}')
+    synonym_results = synonym_word.synonyms()
+
+    # Checking if there is not a synonym
+    if synonym_results == 404:
+        novas_palavras = [search_word]
+        messages.add_message(request, messages.INFO, 'Não há sinônimos para esta palavra!')
+    else:
+        novas_palavras = synonym_results
+        novas_palavras.append(search_word)
+
+    # Main code
+    for i in lista_arquivos:
+        try:
+            caminho = fr"media\files\{i}"  # vitor
+            # caminho = fr"C:\Users\entra21\Desktop\testes\{i}"
+            # caminho = fr"C:\Users\cairo\OneDrive\Área de Trabalho\testes\{i}"  # cairo
+            sum = 0
+            texto = docx2txt.process(caminho)
+            novo_texto = ''.join(ch for ch in unicodedata.normalize('NFKD', texto).lower()
+                                 if not unicodedata.combining(ch))
+
+            total = len(novas_palavras)
+
+            for palavra in novas_palavras:
+                if palavra in novo_texto:
+                    sum += 1
+                    contained_words += f'{palavra}, '
+                else:
+                    continue
+
+            list_contained_words.append(contained_words)
+            contained_words = ''
+            lista_quantidade_palavras.append(sum)
+
+        except:
+            # quantidade = []
+            text = ''
+            sum = 0
+            with fitz.open(caminho) as pdf:
+                for page in pdf:
+                    text = page.get_text()
+            new_text = ''.join(ch for ch in unicodedata.normalize('NFKD', text).lower()
+                               if not unicodedata.combining(ch))
+
+            for j in novas_palavras:
+                if j in new_text:
+                    sum += 1
+                    contained_words += f'{j}, '
+                else:
+                    continue
+
+            list_contained_words.append(contained_words)
+            contained_words = ''
+            lista_quantidade_palavras.append(sum)
+
+    for arquivo, quantidade_palavras, palavras_contidas in zip(lista_arquivos, lista_quantidade_palavras,
+                                                               list_contained_words):
+        final_dict = {'arquivo': arquivo, 'quantidade_palavras': quantidade_palavras,
+                      'palavras_contidas': palavras_contidas}
+
+        # quantidade = [arquivo, quantidade_palavras, total]
+        lista_final.append(final_dict)
+        result = sorted(lista_final, key=itemgetter('quantidade_palavras'), reverse=True)
+
+    for i in result:
+        list_files_name.append(i['arquivo'])
+        list_word_qtd.append(i['quantidade_palavras'])
+        final_contained_words.append(i['palavras_contidas'])
+
+    for files_name, word_qtd, words in zip(list_files_name, list_word_qtd, final_contained_words):
+        complete_list = [files_name, word_qtd, words]
+        real_final.append(complete_list)
+
+    return render(request, 'super_searcher/ranking_synonyms.html', {'real_final': real_final, 'total': total,
+                                                                 'synonym_results': synonym_results,
+                                                                 'no_synonyms': no_synonyms, })
 
 
 def ranking(request):
@@ -320,25 +426,76 @@ def ranking(request):
     total_list = total(request)
     files_name_list = list_files_name(request)
 
-    # # Removing the filtered files
-    # dirPath = r"media/filtered_files"
-    # list_arq = next(os.walk(dirPath))[2]
-    # for i in list_arq:
-    #     os.remove(f"media/filtered_files/{i}")
+    # Removing the filtered files
+    dirPath = r"media/filtered_files"
+    list_arq = next(os.walk(dirPath))[2]
+    for i in list_arq:
+        os.remove(f"media/filtered_files/{i}")
 
-    # # Saving the filtered files in the database
-    # dirPath = r"media\files"
-    # lista_arquivos = next(os.walk(dirPath))[2]
-    #
-    # for aqur in lista_arquivos:
-    #     for filt in files_name_list:
-    #         if aqur == filt:
-    #             shutil.copy2(f'media/files/{aqur}', f'media/filtered_files/{aqur}')
+    # Saving the filtered files in the database
+    dirPath = r"media\files"
+    lista_arquivos = next(os.walk(dirPath))[2]
+
+    for aqur in lista_arquivos:
+        for filt in files_name_list:
+            if aqur == filt:
+                shutil.copy2(f'media/files/{aqur}', f'media/filtered_files/{aqur}')
 
     return render(request, 'super_searcher/ranking.html', {'real_final_list': real_final_list,
                                                            'total_list': total_list,
                                                            'files_name_list': files_name_list})
 
 
+# Receiving the filtered files
+def email_input(request):
+    return render(request, 'super_searcher/email_input.html')
+
+
+def email_response(request):
+    dirPath = r"media/filtered_files"
+    lista_arquivo = next(os.walk(dirPath))[2]
+
+    for i in lista_arquivo:
+        shutil.move(f'media/filtered_files/{i}',
+                    f'media/filtered_files/{i}'.replace("á", "a").replace("é", "e").replace("ê", "e").replace("í", "i").replace("ó", "o")
+                    .replace("ú", "u").replace("ü", "u").replace("ã", "a").replace("ç", "c").replace(" ", "_")
+                    .replace(",", "").replace("õ", "o").replace("Á", "A").replace("É", "E").replace("Ê", "E").replace("Í", "I").replace("Ó", "O")
+                    .replace("Ú", "U").replace("Ü", "U").replace("Ã", "A").replace("Ç", "C").replace(" ", "_")
+                    .replace(",", "").replace("Õ", "O"))
+    dirPath = r"media/filtered_files"
+    list_arq = next(os.walk(dirPath))[2]
+    fromaddr = "mamba.python.entra21@gmail.com"
+    toaddr = str(request.GET.get('term').replace('%40', '@'))
+
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = "Emails Filtrados"
+    body = "Aqui estão seus emails filtrados"
+    msg.attach(MIMEText(body, 'plain'))
+
+    for i in list_arq:
+        filename = i
+        attachment = open(f"media/filtered_files/{i}", "rb")
+        p = MIMEBase('application', 'octet-stream')
+        p.set_payload(attachment.read())
+        encoders.encode_base64(p)
+
+        p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+        msg.attach(p)
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(fromaddr, "pzlyubevjrwgyobq")
+    text = msg.as_string()
+    s.sendmail(fromaddr, toaddr, text)
+    s.quit()
+
+    return render(request, 'super_searcher/email_response.html', {'list_files_name': list_files_name})
+
+
 def about(request):
     return render(request, 'super_searcher/about.html')
+
+
+
