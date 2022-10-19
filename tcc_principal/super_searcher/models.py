f4d65f18fdc3bfc501b4845bbda20662a1f8502f
Cairo Augusto de Andrade <cairo.andrade777@gmail.com>
2022-10-19 12:07:54 -0300

    home testing and fixing

--- tcc_principal/super_searcher/models.py
+++ tcc_principal/super_searcher/models.py
@@ -4,13 +4,13 @@ from django.db import models
 class File(models.Model):
     file = models.FileField(upload_to='files/', null=True)
 
-    def __str__(self):
-        return self.file
+    # def __str__(self):
+    #     return self.file
 
 
 class FilteredFiles(models.Model):
     filtered_files = models.FileField(upload_to='filtered_files/', null=True)
 
-    def __str__(self):
-        return self.filtered_files
+    # def __str__(self):
+    #     return self.filtered_files
 
